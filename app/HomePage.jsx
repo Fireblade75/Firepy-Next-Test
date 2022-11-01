039dfaa30cd0bf0c440014e9195e01db918f9efd
Fireblade75 <Fireblade75@users.noreply.github.com>
2022-11-01 14:07:07 +0100

    initial commit

---
+++ app/HomePage.jsx
@@ -0,0 +1,8 @@
+export default function HomePage({ recentPosts }) {
+    return (
+        <>
+            <h1>Firepy Next Test</h1>
+            <small>This is a serverside component</small>
+        </>
+    )
+}
---
+++ app/layout.js
@@ -0,0 +1,11 @@
+
+export default function RootLayout({children}) {
+    return (
+        <html lang="en">
+            <head>
+                <title>Firepy Next</title>
+            </head>
+            <body>{children}</body>
+        </html>
+    )
+}
